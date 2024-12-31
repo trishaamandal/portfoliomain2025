c05cfff71d0f1f9906fc888a338391e4f3c0f29c
trishaamandal <mandaltrisha39@gmail.com>
2024-12-31 17:51:46 +0530

    Your commit message

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -7,6 +7,8 @@ import Work6 from "../../assets/gymm.png";
 import Work7 from "../../assets/food.png";
 import Work8 from "../../assets/netflix.png";
 import Work9 from "../../assets/1cyber.png";
+import Work10 from "../../assets/health.png";
+
 // import Work10 from "../../assets/gymm.png";
 
 
@@ -87,7 +89,7 @@ const Menu = [
     },
     {
         id: 10,
-        image: Work8,
+        image: Work10,
         title: "Health Vision",
         category: ["Backend"],
         url: "",
